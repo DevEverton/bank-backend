e97a5e6e3d637ec885a474c139cb03598c6be5e8
Everton Carneiro <everton.mercante@gmail.com>
2020-07-07 10:37:14 -0300

    Installed dotenv module

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const accountRouter = require("./routes/accountRouter.js");
 
 app.use(express.json());
 app.use("/accounts", accountRouter);
 
-app.listen(5000, async () => {
-  try {
-    console.log("Api Started!");
-  } catch (err) {
-    console.log(err);
-  }
-});
+app.listen(process.env.PORT || 5000);
+
+// app.listen(5000, async () => {
+//   try {
+//     console.log("Api Started!");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
